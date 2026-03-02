42456eafad9bff494d0e1e40fec2819c41949b5c
Preethi Angelin <preethiangelin1@gmail.com>
2026-03-02 12:12:22 +0530

    refactor test functions to use parameterized tests

--- test_problems_1.py
+++ test_problems_1.py
@@ -1,23 +1,33 @@
+import pytest
 from problems_1 import sum_of_elements,count_even_numbers,find_maximum_number
 
-def test_sum_of_elements():
-    assert sum_of_elements([2,4,7]) == 13
-    assert sum_of_elements([]) == 0
-    assert sum_of_elements([9]) == 9
-    assert sum_of_elements([20,-43,-30]) == -53
-    assert sum_of_elements("123") == 0
+@pytest.mark.parametrize("nums, expected", [
+    ([2,4,7], 13),
+    ([], 0),
+    ([9], 9),
+    ([20,-43,-30], -53),
+    ("123", 0)
+])
+def test_sum_of_elements(nums, expected):
+    assert sum_of_elements(nums) == expected
 
-def test_count_even_numbers():
-    assert count_even_numbers([2,4,9]) == 2
-    assert count_even_numbers([]) == 0
-    assert count_even_numbers([3]) == 0
-    assert count_even_numbers([2]) == 1
-    assert count_even_numbers([-2,-4,7]) == 2
+@pytest.mark.parametrize("nums, expected", [
+    ([2,4,9], 2),
+    ([], 0),
+    ([3], 0),
+    ([2], 1),
+    ([-2,-4,7], 2)
+])
+def test_count_even_numbers(nums, expected):
+    assert count_even_numbers(nums) == expected
 
-def test_find_maximum_number():
-    assert find_maximum_number([8,4,2]) == 8
-    assert find_maximum_number([2]) == 2
-    assert find_maximum_number([]) == None
-    assert find_maximum_number([-9,2,-4]) == 2
-    assert find_maximum_number("123") == None
-
+@pytest.mark.parametrize("nums, expected", [
+    ([8,4,2], 8),
+    ([2], 2),
+    ([], None),
+    ([-9,2,-4], 2),
+    ("123", None)
+])
+def test_find_maximum_number(nums, expected):
+    assert find_maximum_number(nums) == expected
+    
